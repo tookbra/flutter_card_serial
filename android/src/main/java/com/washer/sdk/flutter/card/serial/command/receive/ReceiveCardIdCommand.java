d0384e6ad655610833348ca11fc280ac9c8b9ed0
okboom <tookbar@gmail.com>
2021-03-17 15:00:00 +0800

    fix: reverse card no

--- android/src/main/java/com/washer/sdk/flutter/card/serial/command/receive/ReceiveCardIdCommand.java
+++ android/src/main/java/com/washer/sdk/flutter/card/serial/command/receive/ReceiveCardIdCommand.java
@@ -1,10 +1,19 @@
 package com.washer.sdk.flutter.card.serial.command.receive;
 
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringJoiner;
+
 public class ReceiveCardIdCommand extends ReceiveBase {
 
     private final String result;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public ReceiveCardIdCommand(byte[] allPack, int cmd, byte[] data) {
         super(allPack, cmd, data);
 
@@ -12,6 +21,17 @@ public class ReceiveCardIdCommand extends ReceiveBase {
         for (int i = 0; i < data.length; i++) {
             strData += String.format("%02X ", data[i]);
         }
+
+        if(!strData.equals("")) {
+            String [] arr = strData.split(" ");
+            Collections.reverse(Arrays.asList(arr));
+            StringJoiner joiner = new StringJoiner("");
+            for (String v: arr) {
+                joiner.add(v);
+            }
+            strData = joiner.toString();
+        }
+
         result = strData;
     }
 
